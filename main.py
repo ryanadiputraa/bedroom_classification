d493b08f06c7699a6d80404150ad6de4fee5d4fb
Ryan <ryannadiputraa@gmail.com>
2020-10-13 14:40:31 +0800

    preprocessing data and fixed false dir path

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ zip_ref.extractall(base_path)
 zip_ref.close()
 
 base_dir = '/home/ryan/Documents/Data Science Tutorial/Machine Learning/bedroom_image_classification/images'
-train_dir = os.path.join(base_path, 'train')
-validation_dir = os.path.join(base_path, 'val')
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'val')
 
 
 # create clean room dir on data training dir
@@ -27,3 +27,36 @@ validation_clean_dir = os.path.join(validation_dir, 'clean')
 
 # create messy room dir on data validation dir
 validation_messy_dir = os.path.join(validation_dir, 'messy')
+
+
+# image augmentation
+train_datagen = ImageDataGenerator(
+  rescale=1./255,
+  rotation_range=20,
+  horizontal_flip=True,
+  shear_range=.2,
+  fill_mode='nearest'
+)
+
+test_datagen = ImageDataGenerator(
+  rescale=1./255,
+  rotation_range=20,
+  horizontal_flip=True,
+  shear_range=.2,
+  fill_mode='nearest'
+)
+
+
+train_generator = train_datagen.flow_from_directory(
+  train_dir,   # train data dir
+  target_size=(150, 150),   # change all images resolution to 150x150 pixels
+  batch_size=4,
+  class_mode='binary'   # 2 class classification so its binary
+)
+
+validation_generator = test_datagen.flow_from_directory(
+  validation_dir,   # train data dir
+  target_size=(150, 150),   # change all images resolution to 150x150 pixels
+  batch_size=4,
+  class_mode='binary'   # 2 class classification so its binary
+)
